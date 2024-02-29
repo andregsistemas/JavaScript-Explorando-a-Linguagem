000c59e677197caf4916b721bed7843bca367bfa
dukeossuna <andregsistemas@outlook.com>
2024-02-29 09:40:53 -0300

    Arquivo inicial

---
+++ 1-Meu PrimeiroPrograma.js
@@ -0,0 +1,8 @@
+console.log("Meu primeiro programa: Tabalhando com variaveis");
+
+const idade = 26;
+const idadeSomada = idade + 4;
+
+console.log(`Eu tenho ${idade} anos de idade!`);
+console.log(`Eu tenho ${idade + 2} anos de idade!`);
+console.log(`Eu tenho ${idadeSomada} anos de idade!`);
